0f8b59c4e170b4ea3a0c53a8e8409e2b3f55b7b6
Laknonglak <donovanlak@gmail.com>
2023-08-16 15:55:15 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let yearOfBirth = 1975;
+let currentYear = 2023;
+let age = currentYear - yearOfBirth;
+let possibleAge = age + 1;
+console.log (`I was born in ${yearOfBirth} and now my age ${age} or ${possibleAge}`);
